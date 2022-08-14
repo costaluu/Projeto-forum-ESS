173ed2f49d1d9bcffeccb1379b4d3c3ed533bdc0
unknown <jlcs3@cin.ufpe.br>
2022-08-14 16:39:35 -0300

    fix: responsive home design

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,81 +1,80 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzSpaceModule } from 'ng-zorro-antd/space';
-import { IconDefinition } from '@ant-design/icons-angular';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzStatisticModule } from 'ng-zorro-antd/statistic';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzAvatarModule } from 'ng-zorro-antd/avatar'
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown'
+import { NzMenuModule } from 'ng-zorro-antd/menu'
+import { NzSpaceModule } from 'ng-zorro-antd/space'
+import { IconDefinition } from '@ant-design/icons-angular'
+import { NzCardModule } from 'ng-zorro-antd/card'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzStatisticModule } from 'ng-zorro-antd/statistic'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { NzDividerModule } from 'ng-zorro-antd/divider'
+import { NzTableModule } from 'ng-zorro-antd/table'
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm'
+import { NzTypographyModule } from 'ng-zorro-antd/typography'
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
 import {
-  UserOutline,
-  LogoutOutline,
-  LoginOutline,
-  TeamOutline,
-  HomeOutline,
-  ThunderboltOutline,
-  LockOutline,
-  DownOutline,
-  FileTextOutline,
-} from '@ant-design/icons-angular/icons';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { NewsManagementComponent } from './components/news-management/news-management.component';
+    UserOutline,
+    LogoutOutline,
+    LoginOutline,
+    TeamOutline,
+    HomeOutline,
+    ThunderboltOutline,
+    LockOutline,
+    DownOutline,
+    FileTextOutline,
+} from '@ant-design/icons-angular/icons'
+import { HomeComponent } from './components/home/home.component'
+import { LoginComponent } from './components/login/login.component'
+import { NewsManagementComponent } from './components/news-management/news-management.component'
 
 const icons: IconDefinition[] = [
-  UserOutline,
-  LogoutOutline,
-  LoginOutline,
-  TeamOutline,
-  HomeOutline,
-  ThunderboltOutline,
-  LockOutline,
-  DownOutline,
-  FileTextOutline,
-];
+    UserOutline,
+    LogoutOutline,
+    LoginOutline,
+    TeamOutline,
+    HomeOutline,
+    ThunderboltOutline,
+    LockOutline,
+    DownOutline,
+    FileTextOutline,
+]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    NewsManagementComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NzIconModule.forRoot(icons),
-    AppRoutingModule,
-    NzButtonModule,
-    NzAvatarModule,
-    NzDropDownModule,
-    NzMenuModule,
-    NzSpaceModule,
-    NzCardModule,
-    NzFormModule,
-    NzInputModule,
-    NzStatisticModule,
-    NzGridModule,
-    NzDividerModule,
-    NzTableModule,
-    NzPopconfirmModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomeComponent, LoginComponent, NewsManagementComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NzIconModule.forRoot(icons),
+        AppRoutingModule,
+        NzButtonModule,
+        NzAvatarModule,
+        NzDropDownModule,
+        NzMenuModule,
+        NzSpaceModule,
+        NzCardModule,
+        NzFormModule,
+        NzInputModule,
+        NzStatisticModule,
+        NzGridModule,
+        NzDividerModule,
+        NzTableModule,
+        NzPopconfirmModule,
+        NzTypographyModule,
+        NzToolTipModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
