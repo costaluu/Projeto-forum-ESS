c09a121c76ab40c47d2c0054a1c8135fae4d5b8e
unknown <jlcs3@cin.ufpe.br>
2022-08-15 20:09:09 -0300

    feat: new button on header

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'front-end';
-  logged: Boolean = true;
+export class AppComponent implements OnInit {
+    title = 'front-end'
+    logged: Boolean = false
+
+    userInfo: any = {}
+
+    constructor(private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.activatedRoute.paramMap.subscribe(() => {
+            this.logged = this.isUserLogged()
+
+            if (this.logged) {
+                this.userInfo = JSON.parse(localStorage.getItem('userInfo') as string)
+            }
+        })
+    }
+
+    isUserLogged(): Boolean {
+        if (localStorage.getItem('userInfo') == null) {
+            return false
+        } else {
+            return true
+        }
+    }
 }
--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,33 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  validateForm!: FormGroup;
+    validateForm!: FormGroup
 
-  submitForm(): void {
-    if (this.validateForm.valid) {
-      console.log('submit', this.validateForm.value);
-    } else {
-      Object.values(this.validateForm.controls).forEach((control) => {
-        if (control.invalid) {
-          control.markAsDirty();
-          control.updateValueAndValidity({ onlySelf: true });
+    submitForm(): void {
+        if (this.validateForm.valid) {
+            console.log('submit', this.validateForm.value)
+            localStorage.setItem('userInfo', `{"name":"${this.validateForm.value.userName}","type":${2}}`)
+            this.router.navigateByUrl('/home', { skipLocationChange: true, replaceUrl: true })
+        } else {
+            Object.values(this.validateForm.controls).forEach((control) => {
+                if (control.invalid) {
+                    control.markAsDirty()
+                    control.updateValueAndValidity({ onlySelf: true })
+                }
+            })
         }
-      });
     }
-  }
 
-  constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder, private router: Router) {}
 
-  ngOnInit(): void {
-    this.validateForm = this.fb.group({
-      userName: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-    });
-  }
+    ngOnInit(): void {
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false
+        this.validateForm = this.fb.group({
+            userName: [null, [Validators.required]],
+            password: [null, [Validators.required]],
+        })
+    }
 }
